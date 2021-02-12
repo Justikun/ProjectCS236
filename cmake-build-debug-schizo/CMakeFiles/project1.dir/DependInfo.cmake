d391114437a64e07a1d25b62f92b6162b9b2dcfa
Justikun <justintwlowry@gmail.com>
2021-02-11 20:44:56 -0700

    updating Cmake Files
    I connected to the  BYU CS lab for debugging

---
+++ cmake-build-debug-schizo/CMakeFiles/project1.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.LSKQsAtbMT/Automaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/Automaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/BlockCommentAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/BlockCommentAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/CommentAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/CommentAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/EndOfFileAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/EndOfFileAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/IDAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/IDAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/Lexer.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/Lexer.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/MatcherAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/MatcherAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/NewLineAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/NewLineAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/StringAutomaton.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/StringAutomaton.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/Token.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/Token.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/UndefinedAutomata.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/UndefinedAutomata.cpp.o"
+  "/tmp/tmp.LSKQsAtbMT/main.cpp" "/tmp/tmp.LSKQsAtbMT/cmake-build-debug-schizo/CMakeFiles/project1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-schizo/CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/main.cpp.o"
+  "CMakeFiles/project1.dir/Lexer.cpp.o"
+  "CMakeFiles/project1.dir/Token.cpp.o"
+  "CMakeFiles/project1.dir/Automaton.cpp.o"
+  "CMakeFiles/project1.dir/MatcherAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/NewLineAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/CommentAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/EndOfFileAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/StringAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/BlockCommentAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/IDAutomaton.cpp.o"
+  "CMakeFiles/project1.dir/UndefinedAutomata.cpp.o"
+  "project1.pdb"
+  "project1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
